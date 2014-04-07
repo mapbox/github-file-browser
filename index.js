dd3478fe260d12a3c3a7cb93569a25b2453ccb8c
Tom MacWright <tom@macwright.org>
2014-04-07 17:38:54 -0400

    List user repos, show results over 30

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ module.exports.request = req;
 
 function open() {
     return treeui(treeRequest)
+        .expandable(function(res) {
+            var last = res[res.length - 1];
+            return last.type !== 'blob';
+        })
         .display(function(res) {
             var last = res[res.length - 1];
             return last.name || last.login || last.path;
@@ -23,32 +27,43 @@ function open() {
 
 function treeRequest(tree, callback) {
     if (tree.length === 0) {
-        req('/user/orgs', function(err, res) {
-            callback(null, res[0].map(function(_) {
-                return [_];
-            }));
+        req('/user', function(err, user) {
+            req('/user/orgs', function(err, res) {
+                var orgs = res.map(function(_) {
+                    return [_];
+                });
+                callback(null, [user].concat(orgs));
+            });
         });
     } else if (tree.length === 1) {
-        req('/orgs/' + tree[0].login + '/repos', function(err, res) {
-            callback(null, res[0].map(function(_) {
-                return [tree[0], _];
-            }));
-        });
+        if (tree[0].type === 'User') {
+            req('/users/' + tree[0].login + '/repos', function(err, res) {
+                callback(null, res.map(function(_) {
+                    return [tree[0], _];
+                }));
+            });
+        } else {
+            req('/orgs/' + tree[0].login + '/repos', function(err, res) {
+                callback(null, res.map(function(_) {
+                    return [tree[0], _];
+                }));
+            });
+        }
     } else if (tree.length === 2) {
         req('/repos/' + tree[1].full_name + '/branches', function(err, res) {
-            callback(null, res[0].map(function(_) {
+            callback(null, res.map(function(_) {
                 return [tree[0], tree[1], _];
             }));
         });
     } else if (tree.length === 3) {
         req('/repos/' + tree[1].full_name + '/git/trees/' + tree[2].commit.sha, function(err, res) {
-            callback(null, res[0].tree.map(function(_) {
+            callback(null, res.tree.map(function(_) {
                 return [tree[0], tree[1], tree[2], _];
             }));
         });
     } else if (tree.length > 3) {
         req('/repos/' + tree[1].full_name + '/git/trees/' + tree[3].sha, function(err, res) {
-            callback(null, res[0].tree.map(function(_) {
+            callback(null, res.tree.map(function(_) {
                 return [tree[0], tree[1], tree[2], tree[3]].concat([_]);
             }));
         });
@@ -57,10 +72,16 @@ function treeRequest(tree, callback) {
 
 function req(postfix, callback) {
     var q = queue(1);
-
     q.defer(page, null)
         .awaitAll(function(err, res) {
-            callback(err, res);
+            if (res) {
+                var flat = res.reduce(function(mem, r) {
+                    return mem.concat(r);
+                }, []);
+                callback(err, flat);
+            } else {
+                callback(err);
+            }
         });
 
     function page(url, callback) {
